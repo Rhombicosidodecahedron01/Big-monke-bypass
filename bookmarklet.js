2be0b9fe173705a2bb624c6c40300e4c9404d766
Rhombicosidodecahedron01 <163923800+Rhombicosidodecahedron01@users.noreply.github.com>
2024-05-06 07:28:02 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:fetch("https://raw.githubusercontent.com/rhombicosidodecahedron01/Big-monke-bypass/main/turnoffextension.js").then(r => r.text()).then(r => eval(r)) // made by bypassi
+javascript:fetch("https://raw.githubusercontent.com/Rhombicosidodecahedron01/Big-monke-bypass/main/turnoffextension.js").then(r => r.text()).then(r => eval(r)) // made by bypassi
